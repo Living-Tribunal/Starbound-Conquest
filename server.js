4ac8d66813c5eb571cec372cd09e753aae3fa450
Cody <cjdalton15@gmail.com>
2024-07-14 21:49:23 -0400

    new

--- server.js
+++ server.js
@@ -1,34 +1,63 @@
-const http = require('http');
+const port = 3000;
 const fs = require('fs');
 const express = require('express');
+const http = require('http');
 const path = require('path');
-const port = 3000;
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server, {
+    cors: { origin: "*" }
+});
 
-// Serve static files from the 'public' directory under the '/static' path
-app.use(express.static(path.join(__dirname, '/public')));
+let ships = [];
+
+io.on('connection', (socket) => {
+    console.log(`Socket ${socket.id} connected`);
+
+    socket.emit('initialize', ships);
+
+    socket.on('createShip', (ship) => {
+        console.log('Ship created');
+        ships.push(ship);
+        io.emit('shipCreated', ship); // Broadcast the new ship to all clients
+    });
+
+    socket.on('moveShip', (ship) => {
+        /* console.log('Ship moved:', ship); */
+        // Update the ship position in the server-side array
+        let index = ships.findIndex(s => s.id === ship.id);
+        if (index !== -1) {
+            ships[index] = ship;
+            /* console.log(ships); */
+        }
+        // Broadcast the moved ship to all clients
+        io.emit('shipMoved', ship);
+    });
+ 
+    socket.on('disconnect', () => {
+        console.log(`Socket ${socket.id} disconnected`);
+    });
+});
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Route for serving the index.html file
 app.get('/', function(req, res) {
-    // Read the 'index.html' file from the 'public' directory
     fs.readFile(path.join(__dirname, 'public', 'index.html'), function(error, data) {
         if (error){
-            // Handle file not found error
             res.writeHead(404, {'Content-Type': 'text/plain'});
             res.end('Error: File not Found');
         } else {
-            // Send the contents of the file
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.end(data);
         }
     });
 });
 
-// Create an HTTP server using the Express app
-const server = http.createServer(app);
-
-// Start the server listening on the specified port
+// Start the HTTP server listening on the specified port
 server.listen(port, function(error) {
     if (error){
         console.log('Something went wrong', error);
